bebde8651ac9333bf61290f1f32688d567dc2ac9
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2018-04-25 14:39:29 +0100

    Added request logic

---
+++ client/public/bundle.js
@@ -0,0 +1,85 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./client/src/app.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./client/src/app.js":
+/*!***************************!*\
+  !*** ./client/src/app.js ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("// let countries = [];\n// const restCountriesRequest = new Request(\"https://restcountries.eu/rest/v2/all\");\n//\n// document.addEventListerner('DOMContentLoaded', () => {\n//\n//   const onCountrySelect = function(event){\n//     console.log(event.value);\n//   }\n//\n//\n//\n//\n//\n//   const countriesDropdown = document.querySelector('#countries-dropdown');\n//   countriesDropdown.addEventListener('change', onCountrySelect);\n// });\n//\n// const populateDropdown = function(){\n//   countries = restCountriesRequest.get();\n// }\n\n\n//# sourceURL=webpack:///./client/src/app.js?");
+
+/***/ })
+
+/******/ });
---
+++ client/src/app.js
@@ -0,0 +1,20 @@
+// let countries = [];
+// const restCountriesRequest = new Request("https://restcountries.eu/rest/v2/all");
+//
+// document.addEventListerner('DOMContentLoaded', () => {
+//
+//   const onCountrySelect = function(event){
+//     console.log(event.value);
+//   }
+//
+//
+//
+//
+//
+//   const countriesDropdown = document.querySelector('#countries-dropdown');
+//   countriesDropdown.addEventListener('change', onCountrySelect);
+// });
+//
+// const populateDropdown = function(){
+//   countries = restCountriesRequest.get();
+// }
